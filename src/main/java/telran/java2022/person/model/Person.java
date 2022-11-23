44a74000ca26909a75ec0db12cd3ab5ba5466a22
Dmitri-Krivov <dk4020788@gmail.com>
2022-11-24 00:59:12 +0200

    person-service

---
+++ src/main/java/telran/java2022/person/model/Person.java
@@ -0,0 +1,32 @@
+
+package telran.java2022.person.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@EqualsAndHashCode(of = "id")
+@Entity//saying that Person mapping to table//Necessary#1
+@Table(name = "persons")
+public class Person {
+	@Id//Necessary#2
+	Integer id;
+	@Setter
+	String name;
+	LocalDate birthDate;
+	@Setter
+//	@Embedded
+	Address address;
+
+}
---
+++ src/main/java/telran/java2022/person/service/PersonServiceImpl.java
@@ -0,0 +1,87 @@
+package telran.java2022.person.service;
+
+import java.time.LocalDate;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import telran.java2022.person.dao.PersonRepository;
+import telran.java2022.person.dto.AddressDto;
+import telran.java2022.person.dto.CityPopulationDto;
+import telran.java2022.person.dto.PersonDto;
+import telran.java2022.person.dto.PersonNotFoundException;
+import telran.java2022.person.model.Address;
+import telran.java2022.person.model.Person;
+
+@Service
+@RequiredArgsConstructor
+public class PersonServiceImpl implements PersonService {
+	final PersonRepository personRepository;
+	final ModelMapper modelMapper;
+
+	@Override
+	public Boolean addPerson(PersonDto personDto) {
+		personRepository.save(modelMapper.map(personDto, Person.class));
+		return true;
+	}
+
+	@Override
+	public PersonDto findPersonById(Integer id) {
+		Person person = personRepository.findById(id).orElseThrow(PersonNotFoundException::new);
+		return modelMapper.map(person, PersonDto.class);
+	}
+
+	@Override
+	public PersonDto removePerson(Integer id) {
+		Person person = personRepository.findById(id).orElseThrow(PersonNotFoundException::new);
+		personRepository.delete(person);
+		return modelMapper.map(person, PersonDto.class);
+	}
+
+	@Override
+	public PersonDto updatePersonName(Integer id, String name) {
+		Person person = personRepository.findById(id).orElseThrow(PersonNotFoundException::new);
+		person.setName(name);
+		personRepository.save(person);
+		return modelMapper.map(person, PersonDto.class);
+	}
+
+	@Override
+	public PersonDto updatePersonAddress(Integer id, AddressDto addressDto) {
+		Person person = personRepository.findById(id).orElseThrow(PersonNotFoundException::new);
+		person.setAddress(modelMapper.map(addressDto, Address.class));
+		return modelMapper.map(personRepository.save(person), PersonDto.class);
+	}
+
+	@Override
+	public Iterable<PersonDto> findPersonsByCity(String city) {
+		return personRepository.findAllByAddressCity(city)
+				.map(p -> modelMapper.map(p, PersonDto.class))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public Iterable<PersonDto> findPersonsByName(String name) {
+		return personRepository.findAllByName(name)
+				.map(p -> modelMapper.map(p, PersonDto.class))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public Iterable<PersonDto> findPersonsBetweenAge(Integer minAge, Integer maxAge) {
+		LocalDate max = LocalDate.now().minusYears(minAge);
+		LocalDate min = LocalDate.now().minusYears(maxAge);
+		return personRepository.findAllByBirthDateBetween(min, max)
+				.map(p -> modelMapper.map(p, PersonDto.class))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public Iterable<CityPopulationDto> getCitiesPopulation() {
+		return personRepository.getCitiesPopulation().map(p -> modelMapper
+				.map(p, CityPopulationDto.class))
+				.collect(Collectors.toList());
+	}
+}
